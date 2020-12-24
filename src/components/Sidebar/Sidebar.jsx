8d7313176c4e97eaff15ca301373b69d484e517e
Binay <binayprogramming@gmail.com>
2020-12-24 13:37:06 +0100

    Header and search section of sidebar done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -32,7 +32,9 @@ function Sidebar() {
           <input type='text' placeholder='Search or start new chat' />
         </div>
       </div>
-      <div className='sidebar__chats'></div>
+      <div className='sidebar__chats'>
+        <SidebarChat />
+      </div>
     </div>
   );
 }

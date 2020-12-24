4c60ff93e23a29b873ff37d8e44938d6aff8928f
Binay <binayprogramming@gmail.com>
2020-12-24 14:36:39 +0100

    Sidebar stuff on client side is done

--- src/components/Sidebar/SidebarChat/SidebarChat.jsx
+++ src/components/Sidebar/SidebarChat/SidebarChat.jsx
@@ -3,20 +3,28 @@ import { Avatar } from '@material-ui/core';
 
 import './SidebarChat.scss';
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
   const [seed, setSeed] = useState('');
 
   useEffect(() => {
     setSeed(Math.random() * 232);
   }, []);
 
-  return (
+  const createChat = () => {
+    const newChatRoom = prompt('Please enter a name for the chat room');
+  };
+
+  return !addNewChat ? (
     <div className='sidebarChat'>
       <Avatar src={`https://avatars.dicebear.com/4.5/api/gridy/${seed}.svg`} />
       <div className='sidebarChat__info'>
-        <h1>Room name</h1>
+        <h2>Room name</h2>
         <p>Last message...</p>
       </div>
+    </div>
+  ) : (
+    <div className='sidebarChat' onClick={createChat}>
+      <h2>Add new chat</h2>
     </div>
   );
 }

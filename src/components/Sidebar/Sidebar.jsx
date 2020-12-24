d6cd72b83a5de8717257635ba701e50f07b97438
Binay <binayprogramming@gmail.com>
2020-12-24 13:24:37 +0100

    Sidebar's header completed

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import {
+  ChatOutlined,
+  DonutLargeOutlined,
+  MoreVertOutlined,
+  SearchOutlined,
+} from '@material-ui/icons';
 
 import './Sidebar.scss';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      <div className='sidebar__header'></div>
-      <div className='sidebar__search'></div>
+      <div className='sidebar__header'>
+        <Avatar />
+        <div className='sidebar__headerRight'>
+          <IconButton>
+            <DonutLargeOutlined />
+          </IconButton>
+          <IconButton>
+            <ChatOutlined />
+          </IconButton>
+          <IconButton>
+            <MoreVertOutlined />
+          </IconButton>
+        </div>
+      </div>
+      <div className='sidebar__search'>
+        <div className='siderbar__searchContainer'>
+          <SearchOutlined />
+          <input type='text' placeholder='Search or start new chat' />
+        </div>
+      </div>
       <div className='sidebar__chats'></div>
     </div>
   );
